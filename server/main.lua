71ef178e035d00c2e31361bed213ca582d729125
Quasar <91174081+quasar-scripts@users.noreply.github.com>
2022-09-14 05:02:06 -0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -5,7 +5,7 @@ RegisterCommand("items", function(source, args, rawCommand)
 	function(items)
 		if items and items[1] then 
 			for _, id in ipairs(items) do
-				List = List .. '["' .. id.name .. '"] = {\n    ["name"] = "' .. id.name .. '",\n    ["label"] = "' .. id.label .. '",\n    ["weight"] = ' .. id.weight .. ',\n    ["type"] = "item",\n    ["image"] = "' .. id.name ..'.png",\n    ["unique"] = false,\n    ["useable"] = false,\n    ["shouldClose"] = false,\n    ["combinable"] = nil,\n    ["description"] = ""\n},\n\n'
+				List = List .. '["' .. id.name .. '"] = {\n    ["name"] = "' .. id.name .. '",\n    ["label"] = "' .. id.label .. '",\n    ["weight"] = ' .. id.weight .. ',\n    ["type"] = "item",\n    ["image"] = "' .. id.name ..'.png",\n    ["unique"] = false,\n    ["useable"] = true,\n    ["shouldClose"] = false,\n    ["combinable"] = nil,\n    ["description"] = ""\n},\n\n'
 			end	
 
 			print('Finish migrate items from DB')
